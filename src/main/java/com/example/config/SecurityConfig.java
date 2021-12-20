75e4a4261ff6baf48d2aaa181e65304b4988a3bf
DNdjouomo <dimitri.sime.ndjouomo@grupposinapsi.it>
2021-12-20 18:29:02 +0100

    Spring boot security : link

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static com.example.constants.Constants.*;
+//https://www.invivoo.com/securiser-application-spring-boot-spring-security/ TODO
 
 @Configuration
 @EnableWebSecurity
